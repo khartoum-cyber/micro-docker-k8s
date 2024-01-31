55d0261cef2966786b46e4a4a42a058cbe269213
Bartosz Osmanski <bartosz.osmanski.external@eviden.com>
2024-01-31 08:52:17 +0100

    Consuming API within Client

--- Shopping/Shopping.Client/Program.cs
+++ Shopping/Shopping.Client/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient("ShoppingAPIClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5000/"); // Shopping.API url     
+    //client.BaseAddress = new Uri(Configuration["ShoppingAPIUrl"]);
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
